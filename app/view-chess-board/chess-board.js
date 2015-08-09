adf4dcd4e02d32c1dd21103410c88b5a4d3bafec
Jordan Payne <payne.jc@me.com>
2015-08-08 23:49:03 -0300

    Add scope to chess board view.

--- app/view-chess-board/chess-board.js
+++ app/view-chess-board/chess-board.js
@@ -9,6 +9,10 @@ angular.module('chessApp.chessBoard', ['ngRoute'])
   });
 }])
 
-.controller('ChessBoardCtrl', [function() {
+.controller('ChessBoardCtrl', ['$scope', function(sc) {
 
+  sc.pieces = ['PAWN', 'KNIGHT', 'BISHOP', 'ROOK', 'QUEEN', 'KING']
+  sc.board = 0xffffffffffffff
+  18446744073709552000
+  9007199254740991
 }]);
