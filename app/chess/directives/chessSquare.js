e4cbaaa1262b951b5010d964df981473072c794f
Jordan Payne <payne.jc@me.com>
2015-08-14 18:14:32 -0300

    Move hover styling to css.

--- app/chess/directives/chessSquare.js
+++ app/chess/directives/chessSquare.js
@@ -1,71 +1,56 @@
 'use strict';
 
 function chessSquare($compile, $rootScope, Rules, Board) {
-
   return {
     restrict: 'A',
-    compile: function CompilingFunction($templateElement, $templateAttributes) {
+    link: function(scope, elem, attrs) {
 
-      return function LinkingFunction($scope, $element, $attrs) {
-
-        $scope.$on('movePiece', function(event, args) {
-          if (args.move.from == $scope.id) {
-            $scope.unicode = null;
-            $element.css('-webkit-box-shadow', '');
-            $element.css('-box-shadow', '');
-            $scope.$parent.moves.push($scope.move);
-            $scope.move = null;
-            $scope.selectedPiece = null;
+        scope.$on('movePiece', function(event, args) {
+          if (args.move.from == scope.id) {
+            scope.unicode = null;
+            elem.css('-webkit-box-shadow', '');
+            elem.css('-box-shadow', '');
+            scope.$parent.moves.push(scope.move);
+            scope.move = null;
+            scope.selectedPiece = null;
           }
         });
-        $scope.$on('illegalMove', function(event, args) {
+
+        scope.$on('illegalMove', function(event, args) {
           console.log('ILLEGAL MOVE');
-          if (args.move.from == $scope.id) {
-            $element.css('-webkit-box-shadow', '');
-            $element.css('-box-shadow', '');
-            $scope.move = null;
-            $scope.selectedPiece = null;
+          if (args.move.from == scope.id) {
+            elem.css('-webkit-box-shadow', '');
+            elem.css('-box-shadow', '');
+            scope.move = null;
+            scope.selectedPiece = null;
           }
         });
 
-
-
-        $element.on('click', function() {
-          if ($scope.selectedPiece) {
-            if (Rules.isLegal($scope.move)) {
-              $scope.$parent.turn = 1 - $scope.$parent.turn;
-              $scope.unicode = $scope.selectedPiece;
-              $scope.selectedPiece = null;
-              $scope.move.to = $scope.id;
-              $rootScope.$broadcast('movePiece', { 'move': $scope.move, 'piece': $scope.selectedPiece });
+        elem.on('click', function() {
+          if (scope.selectedPiece) {
+            if (Rules.isLegal(scope.move)) {
+              scope.$parent.turn = 1 - scope.$parent.turn;
+              scope.unicode = scope.selectedPiece;
+              scope.selectedPiece = null;
+              scope.move.to = scope.id;
+              $rootScope.$broadcast('movePiece', { 'move': scope.move, 'piece': scope.selectedPiece });
             } else {
-              // $rootScope.$broadcast('illegalMove', { 'move': $scope.move });
+              // $rootScope.$broadcast('illegalMove', { 'move': scope.move });
             }
-          } else if($scope.unicode) {
-            $element.css('-webkit-box-shadow', 'inset 0 0 15px #000');
-            $element.css('-box-shadow', 'inset 0 0 15px #000000');
-            $scope.selectedPiece = $scope.unicode;
-            $scope.move = {'from': $scope.id, 'capturingPiece': $scope.selectedPiece};
+          } else if(scope.unicode) {
+            elem.css('-webkit-box-shadow', 'inset 0 0 15px #000');
+            elem.css('-box-shadow', 'inset 0 0 15px #000000');
+            scope.selectedPiece = scope.unicode;
+            scope.move = {'from': scope.id, 'capturingPiece': scope.selectedPiece};
           }
 
-          $scope.$apply(function() {
-            $compile($element)($scope);
+          scope.$apply(function() {
+            $compile(elem)(scope);
           })
         });
 
-        $element.on('mouseenter', function() {
-          $element.css('-webkit-box-shadow', 'inset 0 0 15px #000');
-          $element.css('-box-shadow', 'inset 0 0 15px #000000');
-        });
-        $element.on('mouseleave', function() {
-          if ($scope.move == null || $scope.move.from != $scope.id) {
-            $element.css('-webkit-box-shadow', '');
-            $element.css('-box-shadow', '');
-          }
-        });
-
-      };
     }
+
   };
 }
 
