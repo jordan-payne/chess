192e31fe31983e6ccc7c23fc5aaeaf56f3de484d
Jordan Payne <payne.jc@me.com>
2015-08-21 18:07:06 -0300

    add express-mailer to project.

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 var express = require('express');
+var mailer = require('express-mailer');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
+
+mailer.extent(app, {
+  from: 'strandx@gmail.com',
+  host: 'smtp.gmail.com',
+  secureConnection: true,
+  port: 465
+  transportMethod: 'SMTP',
+  auth: {
+    user: 'strandx@gmail.com',
+    pass: 'llap6GGL@22'
+  }
+})
+
+app.get('/mail', function (req, res, next) {
+  app.mailer.send('email', {
+    to: 'payne.jc@me.com',
+    subject: 'I want to share a board with you!'
+  }, function (err) {
+    if (err) {
+      console.log(err);
+      res.send('There was an error sending the email');
+      return;
+    }
+    res.send('Email Sent');
+  });
+});
