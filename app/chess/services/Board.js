f47045c70463573f663d1ad2d67b1338588c2196
Jordan Payne <payne.jc@me.com>
2015-08-21 12:10:32 -0300

    More work to get the app and firebase working together, simplifying the object structure.

--- app/chess/services/Board.js
+++ app/chess/services/Board.js
@@ -1,90 +1,88 @@
 'use strict';
 
-function board(spec) {
+function board($firebaseObject, id) {
 
-  var that = {};
+  var newBoard =  {
+      'a8': {'isSelected': false, 'piece': '\u2656'},
+      'b8': {'isSelected': false, 'piece': '\u2658'},
+      'c8': {'isSelected': false, 'piece': '\u2657'},
+      'd8': {'isSelected': false, 'piece': '\u2655'},
+      'e8': {'isSelected': false, 'piece': '\u2654'},
+      'f8': {'isSelected': false, 'piece': '\u2657'},
+      'g8': {'isSelected': false, 'piece': '\u2658'},
+      'h8': {'isSelected': false, 'piece': '\u2656'},
+      'a7': {'isSelected': false, 'piece': '\u2659'},
+      'b7': {'isSelected': false, 'piece': '\u2659'},
+      'c7': {'isSelected': false, 'piece': '\u2659'},
+      'd7': {'isSelected': false, 'piece': '\u2659'},
+      'e7': {'isSelected': false, 'piece': '\u2659'},
+      'f7': {'isSelected': false, 'piece': '\u2659'},
+      'g7': {'isSelected': false, 'piece': '\u2659'},
+      'h7': {'isSelected': false, 'piece': '\u2659'},
+      'a6': {'isSelected': false, 'piece': '\u0020'},
+      'b6': {'isSelected': false, 'piece': '\u0020'},
+      'c6': {'isSelected': false, 'piece': '\u0020'},
+      'd6': {'isSelected': false, 'piece': '\u0020'},
+      'e6': {'isSelected': false, 'piece': '\u0020'},
+      'f6': {'isSelected': false, 'piece': '\u0020'},
+      'g6': {'isSelected': false, 'piece': '\u0020'},
+      'h6': {'isSelected': false, 'piece': '\u0020'},
+      'a5': {'isSelected': false, 'piece': '\u0020'},
+      'b5': {'isSelected': false, 'piece': '\u0020'},
+      'c5': {'isSelected': false, 'piece': '\u0020'},
+      'd5': {'isSelected': false, 'piece': '\u0020'},
+      'e5': {'isSelected': false, 'piece': '\u0020'},
+      'f5': {'isSelected': false, 'piece': '\u0020'},
+      'g5': {'isSelected': false, 'piece': '\u0020'},
+      'h5': {'isSelected': false, 'piece': '\u0020'},
+      'a4': {'isSelected': false, 'piece': '\u0020'},
+      'b4': {'isSelected': false, 'piece': '\u0020'},
+      'c4': {'isSelected': false, 'piece': '\u0020'},
+      'd4': {'isSelected': false, 'piece': '\u0020'},
+      'e4': {'isSelected': false, 'piece': '\u0020'},
+      'f4': {'isSelected': false, 'piece': '\u0020'},
+      'g4': {'isSelected': false, 'piece': '\u0020'},
+      'h4': {'isSelected': false, 'piece': '\u0020'},
+      'a3': {'isSelected': false, 'piece': '\u0020'},
+      'b3': {'isSelected': false, 'piece': '\u0020'},
+      'c3': {'isSelected': false, 'piece': '\u0020'},
+      'd3': {'isSelected': false, 'piece': '\u0020'},
+      'e3': {'isSelected': false, 'piece': '\u0020'},
+      'f3': {'isSelected': false, 'piece': '\u0020'},
+      'g3': {'isSelected': false, 'piece': '\u0020'},
+      'h3': {'isSelected': false, 'piece': '\u0020'},
+      'a2': {'isSelected': false, 'piece': '\u265F'},
+      'b2': {'isSelected': false, 'piece': '\u265F'},
+      'c2': {'isSelected': false, 'piece': '\u265F'},
+      'd2': {'isSelected': false, 'piece': '\u265F'},
+      'e2': {'isSelected': false, 'piece': '\u265F'},
+      'f2': {'isSelected': false, 'piece': '\u265F'},
+      'g2': {'isSelected': false, 'piece': '\u265F'},
+      'h2': {'isSelected': false, 'piece': '\u265F'},
+      'a1': {'isSelected': false, 'piece': '\u265C'},
+      'b1': {'isSelected': false, 'piece': '\u265E'},
+      'c1': {'isSelected': false, 'piece': '\u265D'},
+      'd1': {'isSelected': false, 'piece': '\u265B'},
+      'e1': {'isSelected': false, 'piece': '\u265A'},
+      'f1': {'isSelected': false, 'piece': '\u265D'},
+      'g1': {'isSelected': false, 'piece': '\u265E'},
+      'h1': {'isSelected': false, 'piece': '\u265C'}
+  }
 
-  spec.boardSquares =
-    {
-      'a8': square({'coordinate': 'a8', 'piece': piece({'code': '\u2656'})}),
-      'b8': square({'coordinate': 'b8', 'piece': piece({'code': '\u2658'})}),
-      'c8': square({'coordinate': 'c8', 'piece': piece({'code': '\u2657'})}),
-      'd8': square({'coordinate': 'd8', 'piece': piece({'code': '\u2655'})}),
-      'e8': square({'coordinate': 'e8', 'piece': piece({'code': '\u2654'})}),
-      'f8': square({'coordinate': 'f8', 'piece': piece({'code': '\u2657'})}),
-      'g8': square({'coordinate': 'g8', 'piece': piece({'code': '\u2658'})}),
-      'h8': square({'coordinate': 'h8', 'piece': piece({'code': '\u2656'})}),
-      'a7': square({'coordinate': 'a7', 'piece': piece({'code': '\u2659'})}),
-      'b7': square({'coordinate': 'b7', 'piece': piece({'code': '\u2659'})}),
-      'c7': square({'coordinate': 'c7', 'piece': piece({'code': '\u2659'})}),
-      'd7': square({'coordinate': 'd7', 'piece': piece({'code': '\u2659'})}),
-      'e7': square({'coordinate': 'e7', 'piece': piece({'code': '\u2659'})}),
-      'f7': square({'coordinate': 'f7', 'piece': piece({'code': '\u2659'})}),
-      'g7': square({'coordinate': 'g7', 'piece': piece({'code': '\u2659'})}),
-      'h7': square({'coordinate': 'h7', 'piece': piece({'code': '\u2659'})}),
-      'a6': square({'coordinate': 'a6', 'piece': piece({'code': '\u0020'})}),
-      'b6': square({'coordinate': 'b6', 'piece': piece({'code': '\u0020'})}),
-      'c6': square({'coordinate': 'c6', 'piece': piece({'code': '\u0020'})}),
-      'd6': square({'coordinate': 'd6', 'piece': piece({'code': '\u0020'})}),
-      'e6': square({'coordinate': 'e6', 'piece': piece({'code': '\u0020'})}),
-      'f6': square({'coordinate': 'f6', 'piece': piece({'code': '\u0020'})}),
-      'g6': square({'coordinate': 'g6', 'piece': piece({'code': '\u0020'})}),
-      'h6': square({'coordinate': 'h6', 'piece': piece({'code': '\u0020'})}),
-      'a5': square({'coordinate': 'a5', 'piece': piece({'code': '\u0020'})}),
-      'b5': square({'coordinate': 'b5', 'piece': piece({'code': '\u0020'})}),
-      'c5': square({'coordinate': 'c5', 'piece': piece({'code': '\u0020'})}),
-      'd5': square({'coordinate': 'd5', 'piece': piece({'code': '\u0020'})}),
-      'e5': square({'coordinate': 'e5', 'piece': piece({'code': '\u0020'})}),
-      'f5': square({'coordinate': 'f5', 'piece': piece({'code': '\u0020'})}),
-      'g5': square({'coordinate': 'g5', 'piece': piece({'code': '\u0020'})}),
-      'h5': square({'coordinate': 'h5', 'piece': piece({'code': '\u0020'})}),
-      'a4': square({'coordinate': 'a4', 'piece': piece({'code': '\u0020'})}),
-      'b4': square({'coordinate': 'b4', 'piece': piece({'code': '\u0020'})}),
-      'c4': square({'coordinate': 'c4', 'piece': piece({'code': '\u0020'})}),
-      'd4': square({'coordinate': 'd4', 'piece': piece({'code': '\u0020'})}),
-      'e4': square({'coordinate': 'e4', 'piece': piece({'code': '\u0020'})}),
-      'f4': square({'coordinate': 'f4', 'piece': piece({'code': '\u0020'})}),
-      'g4': square({'coordinate': 'g4', 'piece': piece({'code': '\u0020'})}),
-      'h4': square({'coordinate': 'h4', 'piece': piece({'code': '\u0020'})}),
-      'a3': square({'coordinate': 'a3', 'piece': piece({'code': '\u0020'})}),
-      'b3': square({'coordinate': 'b3', 'piece': piece({'code': '\u0020'})}),
-      'c3': square({'coordinate': 'c3', 'piece': piece({'code': '\u0020'})}),
-      'd3': square({'coordinate': 'd3', 'piece': piece({'code': '\u0020'})}),
-      'e3': square({'coordinate': 'e3', 'piece': piece({'code': '\u0020'})}),
-      'f3': square({'coordinate': 'f3', 'piece': piece({'code': '\u0020'})}),
-      'g3': square({'coordinate': 'g3', 'piece': piece({'code': '\u0020'})}),
-      'h3': square({'coordinate': 'h3', 'piece': piece({'code': '\u0020'})}),
-      'a2': square({'coordinate': 'a2', 'piece': piece({'code': '\u265F'})}),
-      'b2': square({'coordinate': 'b2', 'piece': piece({'code': '\u265F'})}),
-      'c2': square({'coordinate': 'c2', 'piece': piece({'code': '\u265F'})}),
-      'd2': square({'coordinate': 'd2', 'piece': piece({'code': '\u265F'})}),
-      'e2': square({'coordinate': 'e2', 'piece': piece({'code': '\u265F'})}),
-      'f2': square({'coordinate': 'f2', 'piece': piece({'code': '\u265F'})}),
-      'g2': square({'coordinate': 'g2', 'piece': piece({'code': '\u265F'})}),
-      'h2': square({'coordinate': 'h2', 'piece': piece({'code': '\u265F'})}),
-      'a1': square({'coordinate': 'a1', 'piece': piece({'code': '\u265C'})}),
-      'b1': square({'coordinate': 'b1', 'piece': piece({'code': '\u265E'})}),
-      'c1': square({'coordinate': 'c1', 'piece': piece({'code': '\u265D'})}),
-      'd1': square({'coordinate': 'd1', 'piece': piece({'code': '\u265B'})}),
-      'e1': square({'coordinate': 'e1', 'piece': piece({'code': '\u265A'})}),
-      'f1': square({'coordinate': 'f1', 'piece': piece({'code': '\u265D'})}),
-      'g1': square({'coordinate': 'g1', 'piece': piece({'code': '\u265E'})}),
-      'h1': square({'coordinate': 'h1', 'piece': piece({'code': '\u265C'})})
-    };
-
-    that.get_square = function(squareId) {
-      return spec.boardSquares[squareId];
-    }
-
-    that.select_square = function(squareId) {
-      that.get_square(squareId).select();
-    }
-
-    that.get_board_squares = function() {
-      return spec.boardSquares;
-    }
-
-    return that;
+  var ref = new Firebase('https://chess-project.firebaseio.com/game/' + id);
+  var boardRef = ref.child('board');
+  
+  var obj = $firebaseObject(boardRef);
+  obj.$loaded()
+    .then(function(x) {
+      if (x.$value === null) {
+        for (var key in newBoard) {
+          obj[key] = newBoard[key];
+        }
+        obj.$save();
+      }
+    });
+  return obj;
 
 }
 
--- app/chess/services/move.js
+++ app/chess/services/move.js
@@ -1,21 +1,99 @@
 'use strict';
 
-function move(spec) {
+function Move() {
 
-  var that = {};
+  this.move = function(s, id) {
+    s.board[id].isSelected = !s.board[id].isSelected;
+    var color;
+    switch(s.board[id].piece) {
+      case '\u0020':
+        color = 'NEUTRAL';
+        break;
+      case '\u2654':
+        color = 'WHITE';
+        break;
+      case '\u2655':
+        color = 'WHITE';
+        break;
+      case '\u2656':
+        color = 'WHITE';
+        break;
+      case '\u2657':
+        color = 'WHITE';
+        break;
+      case '\u2658':
+        color = 'WHITE';
+        break;
+      case '\u2659':
+        color = 'WHITE';
+        break;
+      default:
+        color = 'BLACK';
+    }
 
-  that.get_capturing_square = function() {
-    return spec.capturingSquare;
+    s.move.capturingColor = color;
+    switch(s.move.capturingColor) {
+      case s.turn.$value:
+        console.log('It is your turn!');
+        s.move.capturingPiece = s.board[id].piece;
+        s.messages.$add({'text': 'You have selected one of your pieces'});
+    }
+
+    // var capturingColor = that.get_board().get_square(squareId).get_piece().get_color();
+    // var capturingSquare = that.get_move().get_capturing_square();
+    // var selectedSquare = that.get_board().get_square(squareId);
+    //
+    // if (typeof capturingSquare == 'undefined') {
+    //
+    //   switch(capturingColor) {
+    //     case that.get_turn():
+    //       that.get_move().set_capturing_square(that.get_board().get_square(squareId));
+    //       that.add_message('You have selected one of your pieces.');
+    //       break;
+    //     case 'NEUTRAL':
+    //       that.add_message('You must select one of your own pieces.');
+    //       selectedSquare.select();
+    //       break;
+    //     default:
+    //       that.add_message('It is not your turn.');
+    //       selectedSquare.select();
+    //       break;
+    //   }
+    // } else {
+    //
+    //   switch(capturingColor) {
+    //     case that.get_turn():
+    //       that.add_message('You cannot capture your own piece.');
+    //       selectedSquare.select();
+    //       capturingSquare.select();
+    //       break;
+    //     case 'NEUTRAL':
+    //       that.add_message('Capturing blank space.');
+    //       selectedSquare.select();
+    //       capturingSquare.select();
+    //       selectedSquare.set_piece(capturingSquare.get_piece());
+    //       capturingSquare.set_piece(piece({'code': '\u0020'}));
+    //       that.set_turn();
+    //       that.set_move(move({}));
+    //       break;
+    //     default:
+    //       that.add_message('Capturing an enemy piece.');
+    //       selectedSquare.select();
+    //       capturingSquare.select();
+    //       selectedSquare.set_piece(capturingSquare.get_piece());
+    //       capturingSquare.set_piece(piece({'code': '\u0020'}));
+    //       that.set_turn();
+    //       that.set_move(move({}));
+    //   }
+    //
+    // }
+
   }
 
-  that.set_capturing_square = function(square) {
-    spec.capturingSquare = square;
-  }
 
-  return that;
+
 
 }
 
-chess.factory('move', function() {
-  return move;
-});
+
+chess.service('moveService', Move);
